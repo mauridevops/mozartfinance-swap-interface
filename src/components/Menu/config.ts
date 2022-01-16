8beb926e007d4c59735462b04ceb0466e7b46fd8
mauridevops <contacto@mauriciooyola.com>
2022-01-16 19:39:03 -0300

    asd

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -20,39 +20,11 @@ const config: MenuEntry[] = [
       },
     ],
   },
-   /*{
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://mozartfinance.io/farms',
-  },
-  {
-    label: 'Pools',
-    icon: 'PoolIcon',
-    href: 'https://mozartfinance.io/pools',
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Github',
-        href: 'https://github.com/mozartfinance',
-      },
-      {
-        label: 'Docs',
-        href: 'https://mozartfinance.gitbook.io/mozart-finance/',
-      },
-      {
-        label: 'Blog',
-        href: 'https://mozart-finance.medium.com/',
-      },
-    ],
-  },
   {
     label: 'Audit by IB',
     icon: 'TicketIcon',
-    href: 'https://github.com/ImmuneBytes/Smart-Contract-Audit-Reports/blob/main/Mozart%20Finance/Mozart%20Finance%20(PIANO)%20-%20Final%20Audit%20Report.pdf',
-  },*/ 
+    href: 'https://github.com/',
+  }, 
 ]
 
 export default config
